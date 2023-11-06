b7fa26d2d69040d4af832ed18082620d0ecdd86c
Heath Stewart <heath.stewart1@gmail.com>
2023-11-06 09:42:26 -0500

    updated URL to be portal.threater.com

--- globals.py
+++ globals.py
@@ -8,7 +8,7 @@ def init():
     global api_endpoints
 
     # initialize variables
-    base_url = 'https://admin.threater.com/api/v5'
+    base_url = 'https://portal.threater.com/api/v5'
     
     # need these headers to make API call
     headers = {
